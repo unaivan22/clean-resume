bbcb3d3a7f2bd8bb322f5a9df3c6fbcc6fe5d9ee
unaivan <unaivan@gmail.com>
2022-12-09 11:12:43 +0700

    update

--- src/page/Home.js
+++ src/page/Home.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="grid place-items-center bg-zinc-900 pt-32 pb-52">
       <div className='text-white grid grid-cols-1 text-center sm:px-12 xs:px-12'>
-        <h1 className='font-bold xl:text-6xl lg:text-6xl md:text-6xl sm:text-4xl xs:text-4xl mx-auto leading-snug '>I build resume template <br /> that you might be able to use</h1>
+        <h1 className='font-bold xl:text-6xl lg:text-6xl md:text-6xl sm:text-5xl xs:text-5xl mx-auto leading-snug '>I build resume template <br /> that you might be able to use</h1>
         <div className='grid xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-1 xs:grid-cols-1 gap-x-4 gap-y-4  text-center mx-auto mt-12 mb-32'>
           <a href='/' className='bg-emerald-600 px-8 py-4 rounded-2xl font-bold hover:bg-emerald-900'>View All Templates</a>
           <a href='https://github.com/unaivan22/clean-resume' className='bg-emerald-600 px-8 py-4 rounded-2xl font-bold hover:bg-emerald-900'>Clone Templates</a>
