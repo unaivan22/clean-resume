16bb9b9037cf95e41f5950874bb32a7d3b798949
unaivan <unaivan@gmail.com>
2022-12-10 09:53:04 +0700

    update

--- src/page/Home.js
+++ src/page/Home.js
@@ -11,7 +11,7 @@ export default function Home() {
           <a href='/' className='bg-emerald-600 px-8 py-4 rounded-2xl font-base hover:bg-emerald-900'>View All Templates</a>
           <a href='https://github.com/unaivan22/clean-resume' className='bg-emerald-600 px-8 py-4 rounded-2xl font-base hover:bg-emerald-900'>Clone Templates</a>
         </div>
-        <div className='grid xl:grid-cols-4 lg:grid-cols-4 md:grid-cols-4 sm:grid-cols-2 xs:grid-cols-2 gap-x-4 gap-y-4 xl:-rotate-6 lg:-rotate-6 md:-rotate-6 sm:rotate-0 xs:rotate-0 lg:px-24 xl:px-24 md:px-24 sm:px-6 xs:px-6'>
+        <div className='grid xl:grid-cols-4 lg:grid-cols-4 md:grid-cols-4 sm:grid-cols-1 xs:grid-cols-1 gap-x-4 gap-y-4 xl:-rotate-6 lg:-rotate-6 md:-rotate-6 sm:rotate-0 xs:rotate-0 lg:px-24 xl:px-24 md:px-24 sm:px-6 xs:px-6'>
           <a href='/v1' className='bg-white p-2 rounded-2xl hover:scale-110 transition ease-in-out delay-150'>
             <img className='object-fit ' src='extends/media/ResumeA4-1.png' />
             <span className='text-zinc-900 xl:text-medium lg:text-medium md:text-base sm:text-sm xs:text-sm py-12'>Clean Resume</span>
