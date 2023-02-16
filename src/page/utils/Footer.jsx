3ba3d62f2e567faa831b3386deda3a88621356b6
unaivan <unaivan@gmail.com>
2023-02-16 08:28:59 +0700

    update walktour

--- src/page/utils/Footer.jsx
+++ src/page/utils/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <div className='bg-slate-50 mt-32'>
         <div className='container mx-auto py-6 grid gap-x-4'>
-            <p className='text-slate-500 text-sm xs:text-center sm:text-center md:text-left lg:text-left'>© <span>{(new Date().getFullYear())}</span> Crafted in Surabaya by <span className='text-blue underline'><a href='https://dinivannendragunawan.vercel.app/' target='_blank'>Dinivan Nendra</a> </span></p>
+            <p className='text-slate-500 text-sm xs:text-center sm:text-center md:text-left lg:text-left'>© <span>{(new Date().getFullYear())}</span> Crafted in Surabaya by <span className='text-blue underline'><a href='mailto:dinivannendragunawan@gmail.com'>Dinivan Nendra</a> </span></p>
         </div>
     </div>
   )
