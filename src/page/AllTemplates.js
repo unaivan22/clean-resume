9e85f6323e2c4d9fb893cbae5a7453cb594778a0
unaivan <unaivan@gmail.com>
2023-03-01 10:13:07 +0700

    add style

--- src/page/AllTemplates.js
+++ src/page/AllTemplates.js
@@ -11,7 +11,7 @@ export default function AllTemplates() {
   return (
     <div className="grid place-items-center bg-white pt-32 pb-52 min-h-screen">
       <div className='text-white grid grid-cols-1 text-center sm:px-4 xs:px-4'>
-        <h1 className='font-bold xl:text-6xl lg:text-6xl md:text-6xl sm:text-4xl xs:text-4xl mx-auto leading-snug text-zinc-900'>Templates that speed up <br className='2xl:block xl:block md:block sm:hidden xs:hidden' /> creating your resume</h1>
+        <h1 className='font-bold xl:text-6xl lg:text-6xl md:text-6xl sm:text-4xl xs:text-4xl mx-auto leading-snug text-zinc-900'>ATS-Friendly templates that speed up <br className='2xl:block xl:block md:block sm:hidden xs:hidden' /> creating your resume</h1>
         <div className='grid xl:grid-cols-1 lg:grid-cols-1 md:grid-cols-1 sm:grid-cols-1 xs:grid-cols-1 gap-x-4 gap-y-4  text-center mx-auto mt-12 mb-32'>
           <a href='https://github.com/unaivan22/clean-resume' className='bg-emerald-600 px-8 py-4 text-white rounded-2xl font-base hover:bg-emerald-900'>Clone Templates</a>
         </div>
